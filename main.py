fa59b0c3e40ffe497de3792018377573f024bab9
Hristo Vasilev <hristo39@gmail.com>
2022-11-07 15:43:23 +0200

    added docstrings

--- main.py
+++ main.py
@@ -3,13 +3,26 @@ import random
 
 class GenerateMatrix(object):
     def __init__(self):
+        """
+        Constructor method to initialize a class instance.
+        """
         self.matrix = []
 
     def print_matrix(self):
+        """
+        Method that prints the random generated matrix
+
+        :return: None
+        """
         for item in self.matrix:
             print item
 
     def _generate_random_matrix(self):
+        """
+        Protected method to generate a random matrix.
+
+        :return: matrix as a 2D list
+        """
         temp_list = []
         for row in range(height):
             for col in range(width):
@@ -22,6 +35,9 @@ class GenerateMatrix(object):
 
 class MatrixGreedyPath(GenerateMatrix):
     def __init__(self):
+        """
+        Constructor method to initialize a class instance.
+        """
         super(MatrixGreedyPath, self).__init__()
         self.matrix = self._generate_random_matrix()
         self.path = []
@@ -29,6 +45,14 @@ class MatrixGreedyPath(GenerateMatrix):
 
     @staticmethod
     def check_if_indices_out_of_bounds(elem_on_row, elem_on_col):
+        """
+        Method to check if the current index is out of bounds. If it is, the function
+        corrects it, in order to avoid IndexError.
+
+        :param elem_on_row: current row index
+        :param elem_on_col: current column index
+        :return: corrected row and column indices
+        """
         index_correction = 1
         if elem_on_row >= height:
             elem_on_row = height - index_correction
@@ -39,12 +63,26 @@ class MatrixGreedyPath(GenerateMatrix):
 
     @staticmethod
     def check_if_at_matrix_border(curr_index):
+        """
+        Boolean method to check if the current index is at the border.
+
+        :param curr_index: index to check
+        :return: True or False
+        """
         largest = width if width >= height else height
         index_correction = 1
 
         return curr_index == largest - index_correction
 
     def find_path(self):
+        """
+        Method to go through the greedy path, when comparing two indices. The function looks ahead
+        and below the current element in the matrix. If the elements are equal or the element at
+        the same row is greater, the path continues right, otherwise breaks the inner for cycle and
+        continues from the next row at the current index.
+
+        :return: list containing all the elements in the path
+        """
         for row in range(height):
             for col in range(self.curr_col, width):
                 next_element_on_column = col + 1
